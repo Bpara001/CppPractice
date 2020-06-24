c7b58df5a8c120cae146204f5bbdbc9598d56235
Jesse <bpara001@ucr.edu>
2020-06-23 19:27:36 -0700

    -IsUnique

--- Chapter1/1.1/IsUnique.cpp
+++ Chapter1/1.1/IsUnique.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int main(){
-cout<<"test"<<endl;
 
+bool IsUnique(string s){
+
+    if(s.size() > 128 ) {
+        return false;
+    }
+    if(s.size() <2 ){
+        return true;
+    }
+
+    sort(s.begin(),s.end());
+    for(int i =0 ; i<s.size()-1;i++){
+
+        if(s.at(i) == s.at(i+1)){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main(){
+    cout<<"Test 1: "<<endl;
+    string Test= "abcdefga";
+    if(IsUnique(Test)){
+        cout<<"String is Unique"<<endl;
+    }
+    else{
+        cout<<"String is not Unique"<<endl;
+    }
     return 0;
 }
