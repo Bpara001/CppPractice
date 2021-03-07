be60b84c96c37856cf31cb39d1e0a13b53056bb7
Jesse <bpara001@ucr.edu>
2021-03-07 14:20:17 -0800

    -Addition of CMAKE for UniqueStrings

--- LeetCode/UniqueString.cpp
+++ LeetCode/UniqueString.cpp
@@ -1,1 +1,17 @@
 #include <iostream> 
+#include <unordered_map>
+#include <queue>
+#include <string>
+
+
+
+
+
+int main() {
+
+	string s = "aaaabbbbccd";
+	std::cout<<IsUnique(s);
+
+
+	return 0;
+}
