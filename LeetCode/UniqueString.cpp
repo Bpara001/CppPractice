e39a2cb0454a294a7492e4755c78cc95522d5c2f
Jesse <bpara001@ucr.edu>
2021-03-07 17:56:59 -0800

    -greedy algo finished

--- LeetCode/UniqueString.cpp
+++ LeetCode/UniqueString.cpp
@@ -4,14 +4,46 @@
 #include <string>
 
 
+int IsUnique(std::string s) {
+	int total = 0;
+	int	curr = 0;
+	std::unordered_map<char, int> hash;
 
+	for (int i = 0; i < s.size(); i++) {
+		hash[ s.at(i) ] += 1;
+	}
+
+	std::priority_queue<int> q;
+
+	for (std::unordered_map<char, int>::iterator itr = hash.begin(); itr != hash.end(); itr++) {
+		q.emplace(itr->second);
+	}
+
+	while (!q.empty()) {
+		curr = q.top();
+		q.pop();
+		if (q.empty()) {
+			break;
+		}
+		else if ( curr == q.top() && curr !=0 ) {
+			total += 1;
+			curr--;
+			q.emplace(curr);
+		}
+		
+	}
+	
+	return total;
+}
 
 
 int main() {
 
-	string s = "aaaabbbbccd";
+	std::string s = "ceabaacb";
 	std::cout<<IsUnique(s);
 
+	
+	
 
 	return 0;
 }
